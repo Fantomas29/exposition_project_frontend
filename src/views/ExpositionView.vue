d4bcceeeb0a5a857b6efbbfe4812782177c0aa13
Fantomas29 <rochard.johan@gmail.com>
2022-03-18 11:40:17 +0100

    Creation des différentes vues

---
+++ src/views/ExpositionView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Page pour l'exposition</h1>
+  </div>
+</template>
---
+++ src/views/Livre_d_OrView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Page pour le livre d'or
+    </h1>
+  </div>
+</template>
